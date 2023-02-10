cde56a70ffc7e5be5fad58333e51ef99f555428b
nikotoscani <124570750+nikotoscani@users.noreply.github.com>
2023-02-10 20:41:47 +0200

    separate test cases from test.c

--- src/test_s21_math.h
+++ src/test_s21_math.h
@@ -1,5 +1,6 @@
 #include <math.h>
-#include <check.h>
+#include <stdlib.h>
+#include <limits.h>
 #include "s21_math.h"
 
 TCase *s21_tc_abs(void);
@@ -8,9 +9,9 @@ TCase *s21_tc_fabs(void);
 //TCase *s21_tc_acos(void);
 //TCase *s21_tc_asin(void);
 //TCase *s21_tc_atan(void);
-//TCase *s21_tc_ceil(void);
+TCase *s21_tc_ceil(void);
 //TCase *s21_tc_cos(void);
-//TCase *s21_tc_floor(void);
+TCase *s21_tc_floor(void);
 //TCase *s21_tc_fmod(void);
 //TCase *s21_tc_log(void);
 //TCase *s21_tc_pow(void);

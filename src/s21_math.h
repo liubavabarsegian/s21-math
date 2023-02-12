550945b603e00b00900750d1cd764d9219cf0c7c
vikingss <igor_ignatyev@mybook.localdomain>
2023-02-12 21:05:42 +0300

    ADD cos, sin, tan

--- src/s21_math.h
+++ src/s21_math.h
@@ -11,6 +11,8 @@
 #define S21_NEGATIVE_INFINITY = -S21_INFINITY
 #define S21_E 2.71828182845904523536028747
 #define S21_NAN 0.0 / 0.0
+#define S21_PI 3.14159265359
+#define S21_EPS 1e-25
 
 int s21_abs(int x);
 long double s21_ceil(double x);
@@ -22,5 +24,8 @@ long double s21_fmod(double x, double y);
 long double s21_log(double x);
 long double s21_pow(double base, double exp);
 long double s21_pow_int(double base, long long int exp);
+long double s21_sin(double x);
+long double s21_cos(double x);
+long double s21_tan(double x);
 
 #endif

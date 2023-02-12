30ec5e5589e90079fb3cfc0256b2c2793a04e960
vikingss <igor_ignatyev@mybook.localdomain>
2023-02-12 22:18:32 +0300

    Modif exp

--- src/s21_exp.c
+++ src/s21_exp.c
@@ -2,18 +2,27 @@
 
 //для больших отрицательных чисел не работает походу
 long double s21_exp(double x) {
-  if (x == -S21_INFINITY) return 0;
-  if (x == S21_INFINITY) return S21_INFINITY;
-
+    long double result = 1.0;
+    long double sum = 1.0;
+    int i = 1;
+    if (x == -S21_INFINITY) return 0;
+    if (x == S21_INFINITY) return S21_INFINITY;
   long double result = 0;
-  for (int i = 0; i < 200; i++) {
-    result += s21_pow_int(x, i) / (long double)s21_factorial(i);
+  //for (int i = 0; i < 200; i++) {
+    while (s21_fabs(result) > S21_EPS) {
+        result = result * x / i;
+        i += 1;
+        sum += result; 
+        if (sum > DBL_MAX) {
+            sum = S21_INFINITY;
+            break;
+        }
   }
-  return result;
+  return sum;
 }
 
-long long int s21_factorial(int x) {
-  if (x == 1 || x == 0) return 1;
-  if (x > 1) return x * s21_factorial(x - 1);
-  return S21_NAN;
-}
+//long long int s21_factorial(int x) {
+//  if (x == 1 || x == 0) return 1;
+//  if (x > 1) return x * s21_factorial(x - 1);
+//  return S21_NAN;
+//}

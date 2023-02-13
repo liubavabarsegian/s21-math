4644de4c3c9b52430495c5e8529b5782c02676ac
nikotoscani <124570750+nikotoscani@users.noreply.github.com>
2023-02-13 18:56:36 +0200

    fmod pow log sqrt added

--- src/test.c
+++ src/test.c
@@ -5,21 +5,21 @@
 int main(void){
   int failed = 0;
   Suite *s_s21_math = suite_create("s21_math.h");
+
   suite_add_tcase(s_s21_math, s21_tc_abs());
   suite_add_tcase(s_s21_math, s21_tc_exp());
   suite_add_tcase(s_s21_math, s21_tc_fabs());
-  // acos
   //suite_add_tcase(s_s21_math, s21_tc_acos());
   //suite_add_tcase(s_s21_math, s21_tc_asin());
   //suite_add_tcase(s_s21_math, s21_tc_atan());
   suite_add_tcase(s_s21_math, s21_tc_ceil());
   suite_add_tcase(s_s21_math, s21_tc_cos());
   suite_add_tcase(s_s21_math, s21_tc_floor());
-  //suite_add_tcase(s_s21_math, s21_tc_fmod());
-  //suite_add_tcase(s_s21_math, s21_tc_log());
-  //suite_add_tcase(s_s21_math, s21_tc_pow());
+  suite_add_tcase(s_s21_math, s21_tc_fmod());
+  suite_add_tcase(s_s21_math, s21_tc_log());
+  suite_add_tcase(s_s21_math, s21_tc_pow());
   suite_add_tcase(s_s21_math, s21_tc_sin());
-  //suite_add_tcase(s_s21_math, s21_tc_sqrt());
+  suite_add_tcase(s_s21_math, s21_tc_sqrt());
   suite_add_tcase(s_s21_math, s21_tc_tan());
 
   SRunner *runner = srunner_create(s_s21_math);

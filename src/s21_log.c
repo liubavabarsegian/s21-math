b2e9200115e87cc73e98b7493db23aa447f061c9
nikotoscani <124570750+nikotoscani@users.noreply.github.com>
2023-02-13 17:56:21 +0200

    some conditional changes
    some conditional changes in order to pass test #5 where x = -INFINTY and result = NAN is expected

--- src/s21_log.c
+++ src/s21_log.c
@@ -1,9 +1,10 @@
 #include "s21_math.h"
 
 long double s21_log(double x) {
-  if (s21_isinf(x)) return x;
+  //if (s21_isinf(x)) return x;
+  if (x == S21_INFINITY) return x;
   if (x == 0) return -S21_INFINITY;
-  if (x < 0) return S21_NAN;
+  if (x < 0 || x == S21_NEGATIVE_INFINITY) return S21_NAN;
 
   long double result = 0;
   int count = 0;
@@ -18,5 +19,23 @@ long double s21_log(double x) {
     minus *= -1;
   }
   result += count;
+  return result;
+}
+
+long double s21_pow_int(double base, long long int exp) {
+  long double result;
+
+  if (exp == 0) return 1;
+  result = 1;
+  if (exp > 0) {
+    for (long long int i = 1; i <= exp; i++) {
+      result = result * base;
+    }
+  } else {
+    exp *= -1;
+    for (long long int i = 1; i <= exp; i++) {
+      result = result / base;
+    }
+  }
   return result;
 }

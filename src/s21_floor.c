4958d6b2767a91cf2cf958814ccff18d3130966d
nikotoscani <124570750+nikotoscani@users.noreply.github.com>
2023-02-16 18:42:03 +0200

    s21_isnan(x) check added

--- src/s21_floor.c
+++ src/s21_floor.c
@@ -1,9 +1,8 @@
 #include "s21_math.h"
 
 long double s21_floor(double x) {
-  long double result;
-  result = (int)x;
-  if (s21_isinf(x)) {
+  long double result = (long long int)x;
+  if (s21_isnan(x) || s21_isinf(x)) {
     result = (long double)x;
   } else if (x < 0 && result != x) {
     result -= 1;

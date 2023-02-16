c545f53fa9a6022f12c99dc038acb95f691bb353
nikotoscani <124570750+nikotoscani@users.noreply.github.com>
2023-02-16 13:48:28 +0200

    added TEST_EPS, PI, PI/2

--- src/test_s21_math.h
+++ src/test_s21_math.h
@@ -4,6 +4,14 @@
 #include <limits.h>
 #include "s21_math.h"
 
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+#ifndef M_PI_2
+    #define M_PI_2 1.57079632679489661923
+#endif
+#define TEST_EPS 0.000001
+
 TCase *s21_tc_abs(void);
 TCase *s21_tc_exp(void);
 TCase *s21_tc_fabs(void);
